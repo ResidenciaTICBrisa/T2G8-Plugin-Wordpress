787bba2aa274e16b66268756e011fec72d0aba9c
Will <williambernardo838@gmail.com>
2024-04-10 15:05:59 -0300

    Feat: Adicionando funcoes de aprovar e rejeitar na interface

--- plugin-prototipo/includes/admin/formulario-admin-page.php
+++ plugin-prototipo/includes/admin/formulario-admin-page.php
@@ -1,3 +1,30 @@
+<?php
+// Função para aprovar o formulário
+function aprovar_formulario($id) {
+    global $wpdb;
+
+    // Atualiza o status do formulário para 'Aprovado' no banco de dados
+    $query = $wpdb->prepare("UPDATE lc_formulario SET situacao = 'Aprovado' WHERE id = %d", $id);
+    $resultado = $wpdb->query($query);
+
+}
+
+function rejeitar_formulario($id) {
+    global $wpdb;
+
+    // Atualiza o status do formulário para 'Aprovado' no banco de dados
+    $query = $wpdb->prepare("UPDATE lc_formulario SET situacao = 'Negado' WHERE id = %d", $id);
+    $resultado = $wpdb->query($query);
+
+}
+
+// Verifica se o parâmetro "action" foi enviado via GET
+if (isset($_GET['action']) && $_GET['action'] === 'approve' && isset($_GET['id'])) {
+    aprovar_formulario($_GET['id']);
+} else if (isset($_GET['action']) && $_GET['action'] === 'reprove' && isset($_GET['id'])) {
+    rejeitar_formulario($_GET['id']);
+}
+?>
 <!DOCTYPE html>
 <head>
 </head>
@@ -33,7 +60,9 @@
                 echo '<td>' . $dados->descricao . '</td>';
                 echo '<td>' . $dados->data_hora . '</td>';
                 echo '<td>' . $dados->situacao . '</td>';
-                echo '<td><a href="?page=lc_admin&action=delete&id=' . $dados->id . '">Excluir</a></td>';
+                echo '<td><a href="?page=lc_admin&action=approve&id=' . $dados->id . '">Aprovar</a>
+                <a href="?page=lc_admin&action=reprove&id=' . $dados->id . '">Negar</a>
+                <a href="?page=lc_admin&action=delete&id=' . $dados->id . '">Excluir</a></td>';
                 echo '</tr>';
             }
             ?>
