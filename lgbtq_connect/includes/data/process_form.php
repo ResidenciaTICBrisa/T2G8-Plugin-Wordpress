5709d164329dda41996dfc8ec647d2ef0f65aa68
Max-Rohrer20 <maxrohrer2023@gmail.com>
2024-05-27 11:17:40 -0300

    REFACTOR: Alteração do código process_form.php para a refatoração do código

--- lgbtq_connect/includes/data/process_form.php
+++ lgbtq_connect/includes/data/process_form.php
@@ -1,27 +1,29 @@
 <?php
-// Inclui o arquivo WordPressHelper.php usando um caminho relativo
-require_once plugin_dir_path(__FILE__) . 'WordPressHelper.php';
-// Supondo que processar_formulario esteja também nesse arquivo ou você pode colocar em outro arquivo e incluí-lo aqui.
+// Inclui o arquivo Auxiliar_Process_Forms.php usando um caminho relativo
+require_once plugin_dir_path(__FILE__) . 'auxiliar_process_form.php';
+
 function processar_formulario() {
+    global $wpdb;
+
     // Verifica se o formulário foi enviado
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Transforma a string do formData em uma array
         parse_str($_POST['formData'], $formFields);
 
         // Filtra o conteúdo enviado nos formulários
-        $nome = isset($formFields['nome']) ? WordPressHelper::sanitize_data($formFields['nome'], 'text') : '';
-        $email = isset($formFields['email']) ? WordPressHelper::sanitize_data($formFields['email'], 'email') : '';
-        $descricao = isset($formFields['descricao']) ? WordPressHelper::sanitize_data($formFields['descricao'], 'textarea') : ''; 
-        $latitude = isset($formFields['latitude']) ? WordPressHelper::sanitize_data($formFields['latitude'], 'float') : 0;
-        $longitude = isset($formFields['longitude']) ? WordPressHelper::sanitize_data($formFields['longitude'], 'float') : 0;
-        $servico = isset($formFields['servico']) ? WordPressHelper::sanitize_data($formFields['servico'], 'text') : '';
+        $nome = isset($formFields['nome']) ? Auxiliar_Process_Forms::sanitize_data($formFields['nome'], 'text') : '';
+        $email = isset($formFields['email']) ? Auxiliar_Process_Forms::sanitize_data($formFields['email'], 'email') : '';
+        $descricao = isset($formFields['descricao']) ? Auxiliar_Process_Forms::sanitize_data($formFields['descricao'], 'textarea') : ''; 
+        $latitude = isset($formFields['latitude']) ? Auxiliar_Process_Forms::sanitize_data($formFields['latitude'], 'float') : 0;
+        $longitude = isset($formFields['longitude']) ? Auxiliar_Process_Forms::sanitize_data($formFields['longitude'], 'float') : 0;
+        $servico = isset($formFields['servico']) ? Auxiliar_Process_Forms::sanitize_data($formFields['servico'], 'text') : '';
 
         // Verifica se todos os campos necessários estão presentes
         if ($nome && $email && $descricao && $latitude && $longitude && $servico) {
-            $data_hora_envio = WordPressHelper::get_current_time();
+            $data_hora_envio = Auxiliar_Process_Forms::get_current_time();
 
             // Insere os dados no banco de dados
-            $result = WordPressHelper::insert_data_into_db(array(
+            $result = Auxiliar_Process_Forms::insert_data_into_db($wpdb, array(
                 'nome' => $nome,
                 'email' => $email,
                 'descricao' => $descricao,
@@ -32,12 +34,12 @@ function processar_formulario() {
             ));
 
             // Obter e-mail do administrador do site
-            $admin_email = WordPressHelper::get_admin_email();
+            $admin_email = Auxiliar_Process_Forms::get_admin_email();
             // Formata a data e hora para o formato desejado
             $data_hora_formatada = date('d/m/Y H:i', strtotime($data_hora_envio));
 
             // Constrói a URL do painel de administração
-            $admin_panel_url = WordPressHelper::get_admin_panel_url();
+            $admin_panel_url = Auxiliar_Process_Forms::get_admin_panel_url();
 
             // Informações adicionais para o e-mail 
             $local_cadastrado = 'Nome do Local: ' . $nome . "\n";
@@ -48,13 +50,13 @@ function processar_formulario() {
 
             $subject = 'LGBTQ+ Connect - Nova solicitação de plotagem recebida';
             // Envie o e-mail de notificação para o administrador do site
-            WordPressHelper::send_email($admin_email, $subject, $message);
+            Auxiliar_Process_Forms::send_email($admin_email, $subject, $message);
 
             // Envie o e-mail de confirmação para o usuário
             $subject_user = 'LGBTQ+ Connect - Sua solicitação de plotagem foi recebida';
             $message_user = 'Olá! Sua solicitação de plotagem foi recebida. Aqui estão os detalhes:' . "\n" . $local_cadastrado . $tipo_servico . $data_hora_cadastro . 'Você será notificado quando sua solicitação for processada. Obrigado!';
 
-            WordPressHelper::send_email($email, $subject_user, $message_user);
+            Auxiliar_Process_Forms::send_email($email, $subject_user, $message_user);
         } else {
             echo "Erro: Preencha todos os campos corretamente.";
         }
