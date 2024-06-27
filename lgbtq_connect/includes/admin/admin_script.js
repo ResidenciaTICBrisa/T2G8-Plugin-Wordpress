c46953fc19ba6a7c1d0e99b9fe861b849f05b1dc
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2024-06-27 10:18:18 -0300

    realizando o merge da developer na branch editar

--- lgbtq_connect/includes/admin/admin_script.js
+++ lgbtq_connect/includes/admin/admin_script.js
@@ -6,6 +6,7 @@ class Filtro {
     static status = "Todos";
     static nome = "";
     static servico = "todos";
+    static servico = "";
 
     static realizarFiltragem(arr) {
         const self = this;
@@ -13,7 +14,6 @@ class Filtro {
             return (self.checarStatus(formulario) && self.checarNome(formulario) && self.checarServico(formulario));
         });
     }
-
     static checarStatus(formulario) {
         if (this.status !== "Todos") {
             return (this.status == formulario.situacao);
@@ -22,9 +22,9 @@ class Filtro {
             return true;
         }
     }
-    
+
     static checarNome(formulario) {
-        return(formulario.nome.toLowerCase().trim().startsWith(this.nome));
+        return(formulario.nome.toLowerCase().trim().startsWith(this.nome.toLowerCase().trim()));
     }
 
     static checarServico(formulario) {
@@ -35,13 +35,142 @@ class Filtro {
             return true;
         }
     }
+
+    static reiniciarFiltro()
+    {
+        this.status="Todos";
+        this.nome="";
+        this.servico="";
+    }
+}
+
+class Ordenador {
+    static coluna = "nome";
+    static ordem = "asc";
+
+    static realizarOrdenacao(arr) {
+        const self = this;
+        return arr.sort(function (a, b) {
+            const aValue = a[self.coluna].trim().toLowerCase();
+            const bValue = b[self.coluna].trim().toLowerCase();
+
+            return (self.ordem === 'asc') ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        });
+    }
+}
+
+// Classe Singleton com todo os métodos e atributos relacionado a tabela
+class Tabela {
+    static adicionarZero(numero) {
+        return numero < 10 ? '0' + numero : numero;
+    }
+
+    static formatarDataHora(data) {
+        const dia = Tabela.adicionarZero(data.getDate());
+        const mes = Tabela.adicionarZero(data.getMonth() + 1); // Adiciona 1 porque os meses são indexados de 0 a 11
+        const ano = data.getFullYear();
+        const hora = Tabela.adicionarZero(data.getHours());
+        const minutos = Tabela.adicionarZero(data.getMinutes());
+        const segundos = Tabela.adicionarZero(data.getSeconds());
+
+        return `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
+    }
+
+    constructor(arr, tabela) {
+        if(Tabela.instance)
+            return Tabela.instance
+
+        this.arr = arr;
+        this.tabela = tabela;
+        Tabela.instance = this;
+    }
+
+    // Limpa o conteúdo da tabela
+    excluirLinhas() {
+        const tbody = this.tabela.querySelector('tbody');
+        tbody.innerHTML="";
+    }
+
+    // Gera novas linhas na tabela de acordo com a array definida
+    gerarLinhas()
+    {
+        const STATUS_BOTOES = {
+            "Aprovado" : `
+            <button type="button" onclick="confirmarAcao('Tem certeza que quer negar a sugestão?', this.form, 'reprove')">Negar</button>
+            `,
+            "Negado" : `
+            <button type="button" onclick="confirmarAcao('Tem certeza que quer aprovar a sugestão?', this.form, 'approve')">Aprovar</button>
+            `,
+            "Pendente" : `
+            <button type="button" onclick="confirmarAcao('Tem certeza que quer aprovar a sugestão?', this.form, 'approve')">Aprovar</button>
+            <button type="button" onclick="confirmarAcao('Tem certeza que quer negar a sugestão?', this.form, 'reprove')">Negar</button>
+            `
+        }
+        const tbody = this.tabela.querySelector('tbody');
+
+        this.arr.forEach(dados => {
+            const linha = document.createElement('tr');
+            linha.id = "formulario-" + dados.id;
+            let descricao;
+            let data = new Date(dados.data_hora);
+            let dataFormatada = Tabela.formatarDataHora(data);
+            if (dados.descricao.length > 10){
+                descricao = `
+                <span id="descricaoResumida_${dados.id}">${dados.descricao.substring(0, 10)}...</span>
+                <span id="descricaoCompleta_${dados.id}" style="display:none;">${dados.descricao}</span>
+                <button data-id="${dados.id}" onclick="mostrarDescricaoCompleta(${dados.id})">Ver mais</button>
+                `
+            }
+            else {
+                descricao = dados.descricao;
+            }
+
+            let acoes = STATUS_BOTOES[dados.situacao];
+
+            linha.innerHTML = `
+            <td id="formulario-${dados.id}-nome">${dados.nome}</td>
+            <td id="formulario-${dados.id}-email">${dados.email}</td>
+            <td id="formulario-${dados.id}-latitude">${dados.latitude}</td>
+            <td id="formulario-${dados.id}-longitude">${dados.longitude}</td>
+            <td id="formulario-${dados.id}-servico">${dados.servico}</td>
+            <td id="formulario-${dados.id}-descricao">${descricao}</td>
+            <td id="formulario-${dados.id}-data_hora">${dataFormatada}</td>
+            <td id="formulario-${dados.id}-situacao">${dados.situacao}</td>
+            <td id="formulario-${dados.id}-acoes">
+                <form method="post" action="">
+                <input type="hidden" name="id" value="${dados.id}">
+                <input type="hidden" name="action" value="">
+                ${acoes}
+                <button type="button" onclick='abrirModalEdicao(${JSON.stringify(dados)})'>Editar</button>
+                <button type="button" onclick="confirmarAcao('Tem certeza que quer excluir a sugestão?', this.form, 'exclude')">Excluir</button>
+            </td>
+            `;
+            tbody.appendChild(linha);
+        });
+    }
+}
+
+function destacarLinhaTabela(id) {
+    let tabela = document.getElementById("tabela");
+    let linha = document.getElementById(("formulario-" +id));
+
+    if(linha===null) {
+        return;
+    }
+
+    // Loop para remover a linha-destacada de todas as linhas
+    for (let i = 0, row; (row = tabela.rows[i]); i++) {
+        row.classList.remove('linha-destacada');
+    }
+
+    linha.classList.add('linha-destacada'); // Adiciona a classe 'linha-destacada'
+    linha.scrollIntoView({ behavior: 'smooth' }); // Rola a página para a linha
 }
 
 function mostrarDescricaoCompleta(id) {
     var descricaoResumida = document.getElementById('descricaoResumida_' + id);
     var descricaoCompleta = document.getElementById('descricaoCompleta_' + id);
     var botao = document.querySelector('button[data-id="' + id + '"]');
-
     if (descricaoResumida.style.display === 'none') {
         descricaoResumida.style.display = 'inline';
         descricaoCompleta.style.display = 'none';
@@ -51,23 +180,6 @@ function mostrarDescricaoCompleta(id) {
         descricaoCompleta.style.display = 'inline';
         botao.innerText = 'Ver menos';
     }
-}
-function destacarLinhaTabela(id) {
-    var tabela = document.getElementById("tabela");
-    var linha = document.getElementById(id);
-
-    // Loop para remover a linha-destacada de todas as linhas
-    for (var i = 0, row; (row = tabela.rows[i]); i++) {
-        row.classList.remove('linha-destacada');
-    }
-
-    linha.classList.add('linha-destacada'); // Adiciona a classe 'linha-destacada'
-    linha.scrollIntoView({ behavior: 'smooth' }); // Rola a página para a linha
-
-    // Remove a classe linha-destacada depois de um determinado tempo
-    setTimeout(function () {
-        linha.classList.remove('linha-destacada');
-    }, 2000);
 }
 
 function initMapAdmin() {
@@ -136,94 +248,6 @@ function initMapEdit(latitude, longitude, nome, servico, descricao) {
     });
 }
 
-
-function initSortButtons() {
-    // Adiciona um evento de clique aos botões de ordenação
-    var sortButtons = document.querySelectorAll('.sort-btn');
-
-    sortButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var table = button.closest('table');
-            var columnIndex = Array.from(button.parentNode.parentNode.children).indexOf(button.parentNode);
-            var order = button.getAttribute('data-order') || 'asc';
-
-            order = (order === 'asc') ? 'desc' : 'asc';
-            button.setAttribute('data-order', order);
-
-            // Atualiza o ícone do botão de ordenação
-            var icon = button.querySelector('.sort-icon');
-
-            // Remove e adiciona a classe do ícone com base na direção da ordenação
-            if (order === 'asc') {
-                icon.innerHTML = '&#9662;'; // Triângulo para baixo (ordem crescente)
-            } else {
-                icon.innerHTML = '&#9652;'; // Triângulo para cima (ordem decrescente)
-            }
-
-            // Obtém todas as linhas da tabela, exceto a primeira (cabeçalho)
-            var rows = Array.from(table.querySelectorAll('tbody > tr'));
-
-            // Determina o critério de ordenação com base na classe do botão
-            if (button.classList.contains('sort-by-date')) {
-                rows.sort(function(a, b) {
-                    var aValue = new Date(a.children[columnIndex].textContent.trim().replace(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})/, '$3-$2-$1T$4:$5:$6'));
-                    var bValue = new Date(b.children[columnIndex].textContent.trim().replace(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})/, '$3-$2-$1T$4:$5:$6'));
-
-                    return (order === 'asc') ? aValue - bValue : bValue - aValue;
-                });
-            } else if (button.classList.contains('sort-by-email')) {
-                rows.sort(function(a, b) {
-                    var aValue = a.children[columnIndex].textContent.trim().toLowerCase();
-                    var bValue = b.children[columnIndex].textContent.trim().toLowerCase();
-
-                    return (order === 'asc') ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
-                });
-            } else {
-                // Caso padrão: ordenação por texto
-                rows.sort(function(a, b) {
-                    var aValue = a.children[columnIndex].textContent.trim().toLowerCase();
-                    var bValue = b.children[columnIndex].textContent.trim().toLowerCase();
-
-                    return (order === 'asc') ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
-                });
-            }
-
-            // Limpa o conteúdo da tabela antes de reordenar
-            while (table.querySelector('tbody').firstChild) {
-                table.querySelector('tbody').removeChild(table.querySelector('tbody').firstChild);
-            }
-
-            // Reinsere as linhas ordenadas na tabela
-            rows.forEach(function(row) {
-                table.querySelector('tbody').appendChild(row);
-            });
-        });
-    });
-}
-
-// Limpa o conteúdo da tabela
-function excluirLinhas(tabela)
-{
-    while (tabela.querySelector('tbody').firstChild) {
-        tabela.querySelector('tbody').removeChild(tabela.querySelector('tbody').firstChild);
-    }
-}
-
-function adicionarZero(numero) {
-    return numero < 10 ? '0' + numero : numero;
-}
-
-function formatarDataHora(data) {
-    const dia = adicionarZero(data.getDate());
-    const mes = adicionarZero(data.getMonth() + 1); // Adiciona 1 porque os meses são indexados de 0 a 11
-    const ano = data.getFullYear();
-    const hora = adicionarZero(data.getHours());
-    const minutos = adicionarZero(data.getMinutes());
-    const segundos = adicionarZero(data.getSeconds());
-    
-    return `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
-}
-
 function confirmarAcao(mensagem, formulario, acao) {
     // Seleciona o modal e seus elementos
     var modal = document.getElementById('confirmModal');
@@ -254,63 +278,6 @@ function confirmarAcao(mensagem, formulario, acao) {
             modal.style.display = "none";
         }
     };
-}
-
-function gerarLinhas(tabela, arr)
-{
-    const STATUS_BOTOES = {
-        "Aprovado" : `
-        <button type="button" onclick="confirmarAcao('Tem certeza que quer negar a sugestão?', this.form, 'reprove')">Negar</button>
-        `,
-        "Negado" : `
-        <button type="button" onclick="confirmarAcao('Tem certeza que quer aprovar a sugestão?', this.form, 'approve')">Aprovar</button>
-        `,
-        "Pendente" : `
-        <button type="button" onclick="confirmarAcao('Tem certeza que quer aprovar a sugestão?', this.form, 'approve')">Aprovar</button>
-        <button type="button" onclick="confirmarAcao('Tem certeza que quer negar a sugestão?', this.form, 'reprove')">Negar</button>
-        `
-    }
-    var tbody = tabela.querySelector('tbody');
-
-    arr.forEach(dados => {
-        var linha = document.createElement('tr');
-        linha.id = dados.id;
-        var descricao;
-        var data = new Date(dados.data_hora);
-        var dataFormatada = formatarDataHora(data);
-        if (dados.descricao.length > 10){
-            descricao = `
-            <span id="descricaoResumida_${dados.id}">${dados.descricao.substring(0, 10)}...</span>
-            <span id="descricaoCompleta_${dados.id}" style="display:none;">${dados.descricao}</span>
-            <button data-id="${dados.id}" onclick="mostrarDescricaoCompleta(${dados.id})">Ver mais</button>
-            `
-        }
-        else {
-            descricao = dados.descricao;
-        }
-        
-        acoes = STATUS_BOTOES[dados.situacao];
-
-        linha.innerHTML = `
-        <td>${dados.nome}</td>
-        <td>${dados.email}</td>
-        <td>${dados.latitude}</td>
-        <td>${dados.longitude}</td>
-        <td>${dados.servico}</td>
-        <td>${descricao}</td>
-        <td>${dataFormatada}</td>
-        <td>${dados.situacao}</td>
-        <td>
-            <form method="post" action="">
-            <input type="hidden" name="id" value="${dados.id}">
-            <input type="hidden" name="action" value="">
-            ${acoes}
-            <button type="button" onclick='abrirModalEdicao(${JSON.stringify(dados)})'>Editar</button>
-            <button type="button" onclick="confirmarAcao('Tem certeza que quer excluir a sugestão?', this.form, 'exclude')">Excluir</button>
-        </td>
-        `;
-        tbody.appendChild(linha);
-    });
 }
 
 function abrirModalEdicao(dados) {
@@ -468,14 +435,13 @@ function imprimirResultados(resultados) {
         }
     }
 }
+
 function filtrar(elemento) {
     let arr = [];
 
     const filtro_nome = document.getElementById("busca_nome");
     const filtro_servico = document.getElementById("selecao_servico");
-
     const contador_resultados = document.getElementById("contador_resultados");
-
     if (elemento) {
         Filtro.status = elemento.value;
     }
@@ -483,21 +449,61 @@ function filtrar(elemento) {
     if(filtro_nome)
     {
         Filtro.nome = filtro_nome.value.toLowerCase().trim();
+        Filtro.nome = filtro_nome.value
     }
 
     if (filtro_servico)
     {
         Filtro.servico = filtro_servico.value;
     }
-
     arr = Filtro.realizarFiltragem(formularios_todos);
     contador_resultados.innerHTML = `
         <p>${arr.length} resultados encontrados<p>
     `;
 
     const tabela = document.getElementById("tabela");
-    excluirLinhas(tabela);
-    gerarLinhas(tabela, arr);
+    const tabelaObj = new Tabela([], tabela);
+    tabelaObj.arr = arr;
+
+    tabelaObj.excluirLinhas()
+    tabelaObj.gerarLinhas();
+}
+
+function ordenar(elemento) {
+    let order = elemento.getAttribute('data-order') || 'asc';
+
+    order = (order === 'asc') ? 'desc' : 'asc';
+    elemento.setAttribute('data-order', order);
+
+    // Atualiza o ícone do botão de ordenação
+    var icon = elemento.querySelector('.sort-icon');
+
+    // Remove e adiciona a classe do ícone com base na direção da ordenação
+    if (order === 'asc') {
+        icon.innerHTML = '&#9662;'; // Triângulo para baixo (ordem crescente)
+    } else {
+        icon.innerHTML = '&#9652;'; // Triângulo para cima (ordem decrescente)
+    }
+
+    Ordenador.ordem = order;
+
+    // Determina o critério de ordenação com base na classe do botão
+    if (elemento.classList.contains('sort-by-date')) {
+        Ordenador.coluna = "data_hora"
+    }
+    else if (elemento.classList.contains('sort-by-email')) {
+        Ordenador.coluna = "email"
+    }
+    else {
+        // Caso padrão: ordenação por nome
+        Ordenador.coluna = "nome"
+    }
+
+    const tabelaObj = new Tabela([], tabela);
+    tabelaObj.arr = Ordenador.realizarOrdenacao(tabelaObj.arr);
+
+    tabelaObj.excluirLinhas();
+    tabelaObj.gerarLinhas()
 }
 
 // Adiciona um evento de clique a todos os botões de "Ver mais/menos"
@@ -510,6 +516,12 @@ document.querySelectorAll('.ver-mais-btn').forEach(function(button) {
 
 // Inicializa o mapa e os botões de ordenação quando a página carrega
 window.onload = function () {
-    initSortButtons();
     initMapAdmin();
+};
+
+// Exporta as classes
+module.exports = {
+    Filtro,
+    Ordenador,
+    Tabela,
 };
