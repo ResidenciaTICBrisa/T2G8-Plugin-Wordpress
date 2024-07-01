a31bb2aabf03ad22bcfc4181c42194d675a47114
Will <williambernardo838@gmail.com>
2024-07-01 14:30:50 -0300

    style: Mundanca do cursor nos botoes na tela de edicao

--- lgbtq_connect/includes/admin/pagina_administracao.php
+++ lgbtq_connect/includes/admin/pagina_administracao.php
@@ -52,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['action'])) {
         <div id="editPopup" style="display:none;"></div>
         <div id="editModal" style="display:none;">
             <button type="button" id="editFechar" onclick="fecharEditor()">
-                <svg height="30px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="30px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <svg height="30px" id="Layer_1" style="enable-background:new 0 0 512 512; cursor: pointer;" version="1.1" viewBox="0 0 512 512" width="30px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/>
                 </svg>
             </button>
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['action'])) {
 
                 <div class="search_wrapper">
                     <input type="text" id="searchInputFormEdit" placeholder="Pesquise a cidade ou estado...">
-                    <button type="button" onclick="return searchButtonClickedEdit()" class="button_search">Pesquisar</button>
+                    <button type="button" onclick="return searchButtonClickedEdit()" class="button_search" style="cursor: pointer;">Pesquisar</button>
                 </div>
 
                 <div id="listaResultadosEdit"></div>
@@ -85,8 +85,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['action'])) {
                 <input type="hidden" name="longitude" id="editLongitude" required>
 
                 <div id=editDivBotoes>
-                    <button type="button" id="editCancelar" onclick="fecharEditor()">Cancelar</button>
-                    <button type="submit" action="atualizar_formulario($wpdb)" id="editSalvar">Salvar</button>
+                    <button type="button" id="editCancelar" onclick="fecharEditor()" style="cursor: pointer;">Cancelar</button>
+                    <button type="submit" action="atualizar_formulario($wpdb)" id="editSalvar" style="cursor: pointer;">Salvar</button>
                 </div>
             </form>
         </div>
