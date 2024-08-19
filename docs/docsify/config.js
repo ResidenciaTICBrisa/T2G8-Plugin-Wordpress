a0c39dc5ce55bd77dc89bc5da19e4f0066cada24
Will <williambernardo838@gmail.com>
2024-08-19 17:13:33 -0300

    DOCS: Ajuste link para repositorio

--- docs/docsify/config.js
+++
@@ -1,8 +0,0 @@
-window.$docsify = {
-    name: 'Documentação',
-    repo: '',
-    loadNavbar: true,
-    loadSidebar: false,
-    coverpage: false,
-    basePath: '/T2G8-Plugin-Wordpress/docs/docsify/'
-};
